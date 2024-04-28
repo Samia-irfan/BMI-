f2818fad5cc8ee6056f7d50fb8e9c6c5b9bb13cf
Samia-irfan <samiairfan00369@gmail.com>
2024-04-29 01:33:42 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,26 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+const BMi = await inquirer.prompt (
+    [
+       
+         {
+            message: "Enter your height in cm:",
+            type : "number",
+            name : "height"
+         },
+        {
+            message: "Enter your weight in Kg:",
+            type : "number",
+            name : "weight"
+        }
+    ]
+)
+
+let height = BMi.weight / (BMi.height/100)**2
+console.log(`your BMI is ${height}`);
+if (BMi.height <= 0){
+    console.log("Please enter a valid number.");
+}
+if (BMi.height <= 0){
+    console.log("Please enter a valid number.");
+}
